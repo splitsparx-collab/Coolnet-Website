7286f2a35780f85b5cf6a54a6d2c4ffa4da81f44
splitsparx-collab <splitsparx@gmail.com>
2026-01-03 15:38:08 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Coolnet</h1>
+      <p>Fast. Reliable. Connected.</p>
+    </div>
+  )
+}
+
+export default App
